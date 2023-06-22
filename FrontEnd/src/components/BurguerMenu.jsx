d718e621daddd48054ba8d865ef2f662e8f6209b
AyozeJesus <ayozejesusleonsantos@gmail.com>
2023-06-22 20:48:38 +0100

    Mis favoritos + cards

--- FrontEnd/src/components/BurguerMenu.jsx
+++ FrontEnd/src/components/BurguerMenu.jsx
@@ -1,29 +1,57 @@
-import React, { useState, useContext } from "react";
-import { FaUser } from "react-icons/fa";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Link } from "react-router-dom";
 
 const BurgerMenu = () => {
-  const { auth, logoutHandler } = useContext(AuthContext);
+  const { userData, auth, logoutHandler } = useContext(AuthContext);
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      window.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
   return (
     <div style={{ position: "relative", zIndex: 9999 }}>
-      <button onClick={toggleMenu}>
-        <FaUser /> Bienvenido👋
+      <button
+        onClick={toggleMenu}
+        style={{
+          cursor: "pointer",
+          border: "none",
+          background: "none",
+          padding: "0",
+          font: "inherit",
+        }}
+      >
+        <img
+          src="/icono_usuario_header.png"
+          alt="Logo"
+          style={{ width: "30px", height: "30px" }}
+        />
+        {userData.userUsername}
       </button>
-
       {isOpen && (
         <ul
+          ref={menuRef}
           style={{
             position: "absolute",
             top: "100%",
             left: 0,
-            backgroundColor: "white",
+            backgroundColor: "transparent", // Cambiado a transparente
             listStyle: "none",
             padding: 0,
             margin: 0,
--- FrontEnd/src/components/Cards/SenderismoCard.jsx
+++ FrontEnd/src/components/Cards/SenderismoCard.jsx
@@ -1,31 +1,40 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { searchAPI } from "../../services/getRecomendations";
+import SearchResultsComponent from "../SearchResultsComponent";
+import SearchComponent from "../SearchComponent";
 
 const SenderismoCard = () => {
+  const navigate = useNavigate();
+
+  const handleSearch = async () => {
+    const categoria = "Senderismo"; // Cambia la categoría de búsqueda a "Senderismo"
+    const results = await searchAPI("", categoria);
+    navigate("/search-results", { state: { searchResults: results } });
+  };
+
   return (
-    <Link to="/senderismo" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
-      <div style={cardStyle}>
-        <h3 style={titleStyle}>Senderismo</h3>
+    <Link to="/search-results" style={{ textDecoration: "none" }}>
+      <div
+        style={{
+          backgroundImage: `url("/viaje_senderismo.jpg")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          width: "200px",
+          height: "200px",
+          borderRadius: "8px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onClick={handleSearch}
+      >
+        <h3 style={{ fontSize: "24px", color: "#000000", margin: "0" }}>
+          Senderismo
+        </h3>
       </div>
     </Link>
   );
-};
-
-const cardStyle = {
-  backgroundImage: `url("/viaje_senderismo.jpg")`,
-  backgroundSize: "cover", // Hacer que la imagen ocupe el 100% del contenedor sin repetirse
-  backgroundPosition: "center",
-  width: "200px",
-  height: "200px",
-  borderRadius: "8px",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
-
-const titleStyle = {
-  fontSize: "24px",
-  color: "#FFFFFF", // Cambiar a color negro (#000000)
-  margin: "0",
 };
 
 export default SenderismoCard;
